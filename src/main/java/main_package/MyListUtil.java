9d98271ec91123f59c008e73585672865321ba07
johnallec <clegnn95p30c352m@studenti.unical.it>
2021-10-28 09:23:24 +0200

    empty constructor added

--- src/main/java/main_package/MyListUtil.java
+++ src/main/java/main_package/MyListUtil.java
@@ -1,11 +1,25 @@
 package main_package;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class MyListUtil {
 	
 	private List<Integer> list;
+	
+	public MyListUtil() {
+		List<Integer> tmp = new ArrayList<Integer>();
+		Random r = new Random();
+		for(int i=0; i<10; ++i) {
+			int n = r.nextInt(10)+1;
+			
+			if(n % 2 == 0) tmp.add(n);
+			else tmp.add(i+1);
+		}
+		this.list = tmp;
+	}
 	
 	public MyListUtil(List<Integer> list) {
 		this.list = list;
---
+++ src/test/java/main_package/MyListUtilTest.java
@@ -0,0 +1,16 @@
+package main_package;
+
+import org.junit.BeforeClass;
+import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.After;
+
+public class MyListUtilTest {
+	
+	private MyListUtil mlu;
+	
+	@BeforeClass
+	public void init() {
+		mlu = new MyListUtil();
+	}
+}
